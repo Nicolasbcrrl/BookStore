0789df0002679cda7d4eccaf01d253f96a6a30aa
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-09-07 22:20:48 +0200

    creation classe book

---
+++ src/main/java/com/serverprogramming/bookstore/bookstore/domain/Book.java
@@ -0,0 +1,83 @@
+package com.serverprogramming.bookstore.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private String year;
+    private String isbn;
+    private double price;
+
+    public Book(String title, String author, String year, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Double.compare(book.price, price) == 0 && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(year, book.year) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year, isbn, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year='" + year + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/test/java/com/serverprogramming/bookstore/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverprogramming.bookstore.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
