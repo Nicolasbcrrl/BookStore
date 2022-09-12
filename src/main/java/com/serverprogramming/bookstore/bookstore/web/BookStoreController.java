6b0606b2cc7066b924b320b875bad07b40b94e99
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-09-12 17:51:57 +0200

    listPage

--- src/main/java/com/serverprogramming/bookstore/bookstore/web/BookStoreController.java
+++ src/main/java/com/serverprogramming/bookstore/bookstore/web/BookStoreController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class BookStoreController {
     @Autowired
     private BookRepository repository;
-    @RequestMapping(value="/bookList", method=RequestMethod.GET)
-    public String studentList(Model model) {
+    @RequestMapping(value="/booklist", method=RequestMethod.GET)
+    public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "bookList";
     }
