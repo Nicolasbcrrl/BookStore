7716498908c7867760204190889f8ecd143d046b
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-09-12 19:21:25 +0200

    add and delete

--- src/main/java/com/serverprogramming/bookstore/bookstore/web/BookStoreController.java
+++ src/main/java/com/serverprogramming/bookstore/bookstore/web/BookStoreController.java
@@ -1,8 +1,10 @@
 package com.serverprogramming.bookstore.bookstore.web;
+import com.serverprogramming.bookstore.bookstore.domain.Book;
 import com.serverprogramming.bookstore.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,5 +16,23 @@ public class BookStoreController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "bookList";
+    }
+
+    @RequestMapping(value="/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addBook";
+    }
+
+    @RequestMapping(value="/save", method=RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") String bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 }
